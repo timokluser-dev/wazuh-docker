8011033813d98300955af54a752879cdc5c4c9f3
Alberto R <alberpilot@gmail.com>
2022-06-21 16:57:20 +0200

    Used 2 args with ENV

--- build-docker-images/wazuh-manager/config/check_repository.sh
+++ build-docker-images/wazuh-manager/config/check_repository.sh
@@ -1,6 +1,5 @@
 ## Variables
-WAZUH_VERSION=$1
-WAZUH_IMAGE_VERSION=$(echo $WAZUH_VERSION | sed -e 's/\.//g')
+WAZUH_IMAGE_VERSION=$(echo $WAZUH_VER | sed -e 's/\.//g')
 WAZUH_CURRENT_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2- | sed -e 's/\.//g')
 ## If wazuh manager exists in apt dev repository, change variables, if not exit 1
 if [ "$WAZUH_IMAGE_VERSION" -le "$WAZUH_CURRENT_VERSION" ]; then
