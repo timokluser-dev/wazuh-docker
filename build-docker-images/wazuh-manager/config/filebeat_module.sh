ba41e39e3e40f4ef0591aa4f087fe1ce861c5eb8
vcerenu <victor.erenu@wazuh.com>
2024-02-22 09:20:56 -0300

    new filebeat module

--- build-docker-images/wazuh-manager/config/filebeat_module.sh
+++ build-docker-images/wazuh-manager/config/filebeat_module.sh
@@ -22,4 +22,4 @@ fi
 
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm &&\
 yum install -y ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm && rm -f ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm && \
-curl -s https://${REPOSITORY}/filebeat/${WAZUH_FILEBEAT_MODULE} | tar -xvz -C /usr/share/filebeat/module
+curl -s https://github.com/wazuh/internal-devel-requests/files/14373218/wazuh-filebeat-0.4.tar.gz | tar -xvz -C /usr/share/filebeat/module
